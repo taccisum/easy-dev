024ab0d4269d3857d70a008164d9665b6e6e05d7
tac <514162920@qq.com>
2017-11-01 18:12:44 +0800

    1. provide EasyDevException 2. update README.md

---
+++ easy-core/src/main/java/cn/tac/framework/easydev/core/exception/EasyDevException.java
@@ -0,0 +1,9 @@
+package cn.tac.framework.easydev.core.exception;
+
+/**
+ * EasyDev开发框架的异常基类
+ * @author tac
+ * @since 01/11/2017
+ */
+public abstract class EasyDevException extends RuntimeException {
+}
