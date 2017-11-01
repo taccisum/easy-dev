d8710565e6fccb6a79390d6a4470dd0ba6391d9c
tac <514162920@qq.com>
2017-11-01 17:23:51 +0800

    provide SpringUtils

---
+++ easy-core/src/main/java/util/SpringUtils.java
@@ -0,0 +1,73 @@
+package util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+/**
+ * 提供与Spring相关的常用工具
+ * @author : tac
+ * @since : 2017/5/13
+ */
+@Component
+public class SpringUtils implements ApplicationContextAware {
+    public static String applicationName = null;
+    public static String applicationAbbr = null;
+
+    private static ApplicationContext ctx = null;
+
+    public static Object getBean(String beanName) {
+        if (ctx != null) {
+            try {
+                return ctx.getBean(beanName);
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    public static <T> T getBean(String beanName, Class<T> requiredType) {
+        T result = null;
+        if (ctx != null) {
+            try {
+                result = ctx.getBean(beanName, requiredType);
+            } catch (Exception e) {
+            }
+        }
+
+        return result;
+    }
+
+    public static <T> T getBean(Class<T> requiredType) {
+        T result = null;
+        if (ctx != null) {
+            try {
+                result = ctx.getBean(requiredType);
+            } catch (Exception e) {
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        applicationName = applicationContext.getId();
+        ctx = applicationContext;
+        applicationAbbr = getProperty("spring.application.abbr", applicationName);
+    }
+
+    public static String getProperty(String key, String defaultValue) {
+        String result = defaultValue;
+        Environment environment = getBean(Environment.class);
+        if (environment != null) {
+            result = environment.getProperty(key, defaultValue);
+        }
+
+        return result;
+    }
+}
