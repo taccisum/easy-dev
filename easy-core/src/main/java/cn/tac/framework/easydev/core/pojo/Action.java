80995615f6f596c2e9d758dbd57adbd737a06f31
tac <514162920@qq.com>
2017-11-01 17:10:47 +0800

    add functional interface Action

---
+++ easy-core/src/main/java/cn/tac/framework/easydev/core/pojo/Action.java
@@ -0,0 +1,13 @@
+package cn.tac.framework.easydev.core.pojo;
+
+/**
+ * 定义一个无参无返回值的函数接口
+ *
+ * @author : tac
+ * @since : 2017/11/1
+ */
+
+@FunctionalInterface
+public interface Action {
+    void doAction();
+}
