a9099f279e302a0478baff9a4f8da6145346ac6d
tac <514162920@qq.com>
2017-11-01 17:23:57 +0800

    provide IDUtils

---
+++ easy-core/src/main/java/util/IDUtils.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.util.UUID;
+
+/**
+ * ID生成器,用于生成ID
+ *
+ * @author tac
+ * @since 02/08/2017
+ */
+public abstract class IDUtils {
+    public static String UUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+}
