39fb60f4cc4b5db4c71cf2efd718d9c09f81e918
tac <514162920@qq.com>
2017-11-16 00:25:48 +0800

    Action unit test

---
+++ easy-core/src/test/java/cn/tac/framework/easydev/core/pojo/ActionTest.java
@@ -0,0 +1,19 @@
+package cn.tac.framework.easydev.core.pojo;
+
+import org.junit.Test;
+
+/**
+ * @author tac
+ * @since 16/11/2017
+ */
+public class ActionTest {
+    @Test
+    public void doAction() throws Exception {
+        doGreetAction(() -> System.out.println("hello world"));
+    }
+
+    private void doGreetAction(Action action) {
+        action.doAction();
+    }
+
+}
