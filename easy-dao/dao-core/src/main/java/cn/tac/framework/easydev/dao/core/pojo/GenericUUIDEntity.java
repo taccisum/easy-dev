886dde580ad3a917a7b7f47902f986207f5c0f2d
tac <514162920@qq.com>
2017-11-15 20:08:39 +0800

    provide uuid primary key entity base class

---
+++ easy-dao/dao-core/src/main/java/cn/tac/framework/easydev/dao/core/pojo/GenericUUIDEntity.java
@@ -0,0 +1,16 @@
+package cn.tac.framework.easydev.dao.core.pojo;
+
+import cn.tac.framework.easydev.core.util.IDUtils;
+
+/**
+ * 通用的UUID主键的实体基类
+ *
+ * @author tac
+ * @since 15/11/2017
+ */
+public abstract class GenericUUIDEntity extends GenericEntity<String> {
+    @Override
+    protected String newId() {
+        return IDUtils.UUID();
+    }
+}
