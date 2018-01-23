c9e362364897389904ee152afd964dadaa0f7d8d
tac <514162920@qq.com>
2018-01-23 11:14:37 +0800

    modify prompt

--- easy-core/src/main/java/cn/tac/framework/easydev/core/exception/BeanUtilsException.java
+++ easy-core/src/main/java/cn/tac/framework/easydev/core/exception/BeanUtilsException.java
@@ -1,11 +1,11 @@
 package cn.tac.framework.easydev.core.exception;
 
 /**
- * @author : tac
- * @since : 02/08/2017
+ * @author tac
+ * @since 1.0
  */
 public class BeanUtilsException extends RuntimeException {
     public BeanUtilsException(String method, Exception cause) {
-        super("BeanUtils." + method + "()执行异常", cause);
+        super("exception happened when invoke BeanUtils." + method + "()", cause);
     }
 }
