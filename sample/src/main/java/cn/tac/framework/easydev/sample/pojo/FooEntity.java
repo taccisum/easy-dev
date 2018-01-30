57237cfac7c74a9902bcf9c3f63d389d7f124fcd
tac <514162920@qq.com>
2018-01-30 23:23:57 +0800

    fix bug

--- sample/src/main/java/cn/tac/framework/easydev/sample/pojo/FooEntity.java
+++ sample/src/main/java/cn/tac/framework/easydev/sample/pojo/FooEntity.java
@@ -1,8 +1,10 @@
 package cn.tac.framework.easydev.sample.pojo;
 
 import cn.tac.framework.easydev.dao.core.pojo.GenericMinEntity;
+import cn.tac.framework.easydev.dao.core.pojo.InitializingEntity;
 import cn.tac.framework.easydev.dao.core.strategy.id.IDGenerator;
 import cn.tac.framework.easydev.dao.core.strategy.id.UUIDGenerator;
+import cn.tac.framework.easydev.dao.core.util.EntityUtils;
 import com.google.common.base.MoreObjects;
 
 import javax.persistence.Table;
@@ -12,7 +14,7 @@ import javax.persistence.Table;
  * @since 03/12/2017
  */
 @Table(name = "foo")
-public class FooEntity extends GenericMinEntity<String> {
+public class FooEntity extends GenericMinEntity<String> implements InitializingEntity {
     private String bar1;
 
     public String getBar1() {
@@ -34,5 +36,10 @@ public class FooEntity extends GenericMinEntity<String> {
                 .add("id", getId())
                 .add("bar1", bar1)
                 .toString();
+    }
+
+    @Override
+    public void init() {
+        EntityUtils.init(this);
     }
 }
