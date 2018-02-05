d1099d594c4249b3fc766e26812400271f8ef4ff
tac <514162920@qq.com>
2018-02-05 16:17:08 +0800

    fix bug

--- easy-core/src/main/java/cn/tac/framework/easydev/core/domain/converter/ConverterUtils.java
+++ easy-core/src/main/java/cn/tac/framework/easydev/core/domain/converter/ConverterUtils.java
@@ -10,30 +10,30 @@ import java.util.List;
  * @since 05/02/2018
  */
 public abstract class ConverterUtils {
-    protected static <FROM, TO> TO convert(FROM from, Class<? extends FROM> fromClazz, Class<TO> toClazz) {
+    public static <FROM, TO> TO convert(FROM from, Class<? extends FROM> fromClazz, Class<TO> toClazz) {
         return (TO) getConverter(fromClazz, toClazz).convert(from);
     }
 
-    protected static <FROM, TO> TO convert(FROM from, Class<TO> toClazz) {
+    public static <FROM, TO> TO convert(FROM from, Class<TO> toClazz) {
         return convert(from, from.getClass(), toClazz);
     }
 
-    protected static <FROM, TO> TO convert(FROM from, TO to) {
+    public static <FROM, TO> TO convert(FROM from, TO to) {
         return (TO) convert(from, to.getClass());
     }
 
-    protected static <FROM, TO> TO convert(FROM from, Class<? extends FROM> fromClazz, TO to) {
+    public static <FROM, TO> TO convert(FROM from, Class<? extends FROM> fromClazz, TO to) {
         return (TO) convert(from, fromClazz, to.getClass());
     }
 
-    protected static <FROM, TO> List<TO> convertAll(List<FROM> fromList, Class<TO> toCls) {
+    public static <FROM, TO> List<TO> convertAll(List<FROM> fromList, Class<TO> toCls) {
         if (CollectionUtils.isEmpty(fromList)) {
             return new ArrayList<>();
         }
         return getConverter(fromList.get(0).getClass(), toCls).convertAll(fromList);
     }
 
-    protected static Converter getConverter(Class fromCls, Class toCls) {
+    public static Converter getConverter(Class fromCls, Class toCls) {
         return ConverterFactory.find(fromCls, toCls);
     }
 }
