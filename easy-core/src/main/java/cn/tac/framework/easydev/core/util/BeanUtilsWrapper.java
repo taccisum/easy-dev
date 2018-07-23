5e8645a90986a2463e34deca4c49c80631e5ace2
tac <514162920@qq.com>
2018-07-23 19:25:37 +0800

    support util method: BeanUtilWrapper.trimAllStringField()

--- easy-core/src/main/java/cn/tac/framework/easydev/core/util/BeanUtilsWrapper.java
+++ easy-core/src/main/java/cn/tac/framework/easydev/core/util/BeanUtilsWrapper.java
@@ -6,6 +6,9 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -16,10 +19,9 @@ import java.util.Set;
  * 1. 对bean utils的调用做封装，若后续对性能有要求，可以很方便替换成更高效的bean utils<br/>
  * 2. 可以根据需求很方便地集成不同的第三方BeanUtils方法到该类
  *
+ * @author tac
  * @see org.springframework.beans.BeanUtils
  * @see org.apache.commons.beanutils.BeanUtils
- *
- * @author tac
  * @since 1.0
  */
 
@@ -43,7 +45,7 @@ public abstract class BeanUtilsWrapper {
     /**
      * 将bean的字段抽取出来作为一个map，暂不支持嵌套对象的抽取（嵌套对象的字段会被忽略）
      */
-    public static Map<String, Object> extract(Object bean){
+    public static Map<String, Object> extract(Object bean) {
         Map<String, Object> params = new HashMap<>(0);
         try {
             PropertyUtilsBean propertyUtilsBean = new PropertyUtilsBean();
@@ -52,7 +54,7 @@ public abstract class BeanUtilsWrapper {
                 String name = descriptors[i].getName();
                 if (!"class".equals(name)) {
                     params.put(name, propertyUtilsBean.getNestedProperty(bean, name));
-                }else{
+                } else {
                     //todo:: 抽取嵌套对象属性
                 }
             }
@@ -62,16 +64,86 @@ public abstract class BeanUtilsWrapper {
         return params;
     }
 
-    private static String[] getNullPropertyNames (Object source) {
+    private static String[] getNullPropertyNames(Object source) {
         final BeanWrapper src = new BeanWrapperImpl(source);
         PropertyDescriptor[] pds = src.getPropertyDescriptors();
 
         Set<String> emptyNames = new HashSet<>();
-        for(PropertyDescriptor pd : pds) {
+        for (PropertyDescriptor pd : pds) {
             Object srcValue = src.getPropertyValue(pd.getName());
             if (srcValue == null) emptyNames.add(pd.getName());
         }
         String[] result = new String[emptyNames.size()];
         return emptyNames.toArray(result);
+    }
+
+
+    /**
+     * trim bean中所有类型为字符串的字段
+     *
+     * @param bean 要操作的bean
+     */
+    public static void trimAllStringField(Object bean) {
+        trimAllStringField(bean, true);
+    }
+
+    /**
+     * @param containNonPublic 是否包含non-public字段，false则只trim public及带有getter、setter的字段
+     */
+    public static void trimAllStringField(Object bean, boolean containNonPublic) {
+        try {
+            if (bean != null) {
+                Field[] fields = bean.getClass().getDeclaredFields();
+                for (int i = 0; i < fields.length; i++) {
+                    Field f = fields[i];
+                    if (f.getType().isAssignableFrom(String.class)) {
+                        String fieldName = f.getName();
+                        String value = getStringFieldValue(bean, fieldName, containNonPublic);
+                        if (value == null) continue;
+                        setStringFieldValue(bean, fieldName, value.trim(), containNonPublic);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new BeanUtilsException("trimAllStringField", e);
+        }
+    }
+
+    private static String getStringFieldValue(Object bean, String fieldName, boolean containNonPublic) throws Exception {
+        try {
+            //try getter
+            String methodName = "get" +
+                    fieldName.substring(0, 1).toUpperCase() +
+                    fieldName.substring(1);
+            Method method = bean.getClass().getMethod(methodName);
+            return method.invoke(bean).toString();
+        } catch (NoSuchMethodException e) {
+            Field field = bean.getClass().getDeclaredField(fieldName);
+            if (containNonPublic || field.getModifiers() == Modifier.PUBLIC) {
+                return field.get(bean).toString();
+            }
+            return null;
+        }
+    }
+
+    private static void setStringFieldValue(Object bean, String fieldName, String value, boolean containNonPublic) throws Exception {
+        try {
+            // try setter
+            String setterName = "set" +
+                    fieldName.substring(0, 1).toUpperCase() +
+                    fieldName.substring(1);
+            Class[] classArr = new Class[1];
+            classArr[0] = "java.lang.String".getClass();
+            Method method;
+            method = bean.getClass().getMethod(setterName, classArr);
+            method.invoke(bean, value);
+        } catch (NoSuchMethodException e) {
+            Field field = bean.getClass().getDeclaredField(fieldName);
+            if (containNonPublic || field.getModifiers() == Modifier.PUBLIC) {
+                field.setAccessible(true);
+                field.set(bean, value);
+                field.setAccessible(false);
+            }
+        }
     }
 }
--- easy-core/src/test/java/cn/tac/framework/easydev/core/util/BeanUtilsWrapperTest.java
+++ easy-core/src/test/java/cn/tac/framework/easydev/core/util/BeanUtilsWrapperTest.java
@@ -1,10 +1,13 @@
 package cn.tac.framework.easydev.core.util;
 
+import com.google.common.base.MoreObjects;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author tac
@@ -49,6 +52,27 @@ public class BeanUtilsWrapperTest {
         Assert.assertEquals("bar2", map.get("bar2"));
     }
 
+    @Test
+    public void trimAllString() throws Exception {
+        Foo4Trim foo = new Foo4Trim(" 1 ", " 2 ", " 3 ", " 4 ", 5);
+        BeanUtilsWrapper.trimAllStringField(foo);
+        assertThat(foo.getBar1()).isEqualTo("1");
+        assertThat(foo.bar2).isEqualTo("2");
+        assertThat(foo.bar3).isEqualTo("3");
+        assertThat(foo.bar4).isEqualTo("4");
+        assertThat(foo.bar5).isEqualTo(5);
+    }
+
+    @Test
+    public void trimAllStringWithoutNonPublic() throws Exception {
+        Foo4Trim foo = new Foo4Trim(" 1 ", " 2 ", " 3 ", " 4 ", 5);
+        BeanUtilsWrapper.trimAllStringField(foo, false);
+        assertThat(foo.getBar1()).isEqualTo("1");
+        assertThat(foo.bar2).isEqualTo(" 2 ");
+        assertThat(foo.bar3).isEqualTo(" 3 ");
+        assertThat(foo.bar4).isEqualTo("4");
+    }
+
     public class Foo {
         public Foo() {
         }
@@ -76,6 +100,49 @@ public class BeanUtilsWrapperTest {
         public void setBar2(String bar2) {
             this.bar2 = bar2;
         }
+
+        @Override
+        public String toString() {
+            return MoreObjects.toStringHelper(this)
+                    .add("bar1", bar1)
+                    .add("bar2", bar2)
+                    .toString();
+        }
     }
 
+    public static class Foo4Trim {
+        private String bar1;
+        protected String bar2;
+        String bar3;
+        public String bar4;
+        private Integer bar5;
+
+        public Foo4Trim(String bar1) {
+            this.bar1 = bar1;
+        }
+
+        public Foo4Trim(String bar1, String bar2, String bar3, String bar4, Integer bar5) {
+            this.bar1 = bar1;
+            this.bar2 = bar2;
+            this.bar3 = bar3;
+            this.bar4 = bar4;
+            this.bar5 = bar5;
+        }
+
+        public String getBar1() {
+            return bar1;
+        }
+
+        public void setBar1(String bar1) {
+            this.bar1 = bar1;
+        }
+
+        public Integer getBar5() {
+            return bar5;
+        }
+
+        public void setBar5(Integer bar5) {
+            this.bar5 = bar5;
+        }
+    }
 }
