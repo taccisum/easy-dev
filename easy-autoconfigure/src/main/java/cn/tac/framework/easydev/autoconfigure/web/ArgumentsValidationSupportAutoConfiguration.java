e827b796c5d4c0ed4e73b2b3fe5fe024d7d18ce8
tac <514162920@qq.com>
2018-08-24 10:05:24 +0800

    optimize code

--- easy-autoconfigure/src/main/java/cn/tac/framework/easydev/autoconfigure/web/ArgumentsValidationSupportAutoConfiguration.java
+++ easy-autoconfigure/src/main/java/cn/tac/framework/easydev/autoconfigure/web/ArgumentsValidationSupportAutoConfiguration.java
@@ -1,6 +1,6 @@
 package cn.tac.framework.easydev.autoconfigure.web;
 
-import cn.tac.framework.easydev.core.CommonErrorCode;
+import cn.tac.framework.easydev.core.exception.biz.ArgumentsValidatingException;
 import cn.tac.framework.easydev.web.argsvalid.ErrorsHandler;
 import cn.tac.framework.easydev.web.argsvalid.handler.MultiErrorsHandler;
 import cn.tac.framework.easydev.web.core.builder.RestfulApiResponseBuilder;
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @ConditionalOnClass(ErrorsHandler.class)
 @RestControllerAdvice
 public class ArgumentsValidationSupportAutoConfiguration {
+    private ArgumentsValidatingException argumentsValidatingException = new ArgumentsValidatingException("");
     @Bean
     @ConditionalOnMissingBean
     public ErrorsHandler errorsHandler() {
@@ -36,20 +37,26 @@ public class ArgumentsValidationSupportAutoConfiguration {
         this.handler = handler;
     }
 
+    /**
+     * @since 2.3.1
+     */
     @ExceptionHandler(value = BindException.class)
     public RestfulApiResponse bindException(BindException e) {
         return RestfulApiResponseBuilder.failure()
-                .code(CommonErrorCode.ARGUMENTS_VALIDATION.getCode())
-                .msg(CommonErrorCode.ARGUMENTS_VALIDATION.getMessage())
+                .code(argumentsValidatingException.getErrorCode().getCode())
+                .msg(argumentsValidatingException.getErrorCode().getMessage())
                 .friendlyMsg(handler.extractMsg(e))
                 .build();
     }
 
+    /**
+     * @since 2.3.1
+     */
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public RestfulApiResponse bindException(MethodArgumentNotValidException e) {
         return RestfulApiResponseBuilder.failure()
-                .code(CommonErrorCode.ARGUMENTS_VALIDATION.getCode())
-                .msg(CommonErrorCode.ARGUMENTS_VALIDATION.getMessage())
+                .code(argumentsValidatingException.getErrorCode().getCode())
+                .msg(argumentsValidatingException.getErrorCode().getMessage())
                 .friendlyMsg(handler.extractMsg(e.getBindingResult()))
                 .build();
     }
