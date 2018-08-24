f54cfb3e1f930851c948c4c1438434cbf5e19c7a
tac <514162920@qq.com>
2018-08-24 14:09:47 +0800

    fix bug: BusinessException will lose exception info because it has wrapped by DecodeException when thrown in feign decoder

---
+++ easy-autoconfigure/src/main/java/cn/tac/framework/easydev/autoconfigure/web/FeignClientSupportAutoConfiguration.java
@@ -0,0 +1,46 @@
+package cn.tac.framework.easydev.autoconfigure.web;
+
+import cn.tac.framework.easydev.core.exception.BusinessException;
+import cn.tac.framework.easydev.core.pojo.ErrorMessage;
+import cn.tac.framework.easydev.core.util.ExceptionUtils;
+import cn.tac.framework.easydev.web.client.feign.config.FeignClientSupportProperties;
+import cn.tac.framework.easydev.web.core.builder.RestfulApiResponseBuilder;
+import cn.tac.framework.easydev.web.core.pojo.RestfulApiResponse;
+import cn.tac.framework.easydev.web.exception.handler.config.WebExceptionHandlerProperties;
+import feign.FeignException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author tac
+ * @since 2.3.1
+ */
+@ConditionalOnClass(FeignClientSupportProperties.class)
+@RestControllerAdvice
+public class FeignClientSupportAutoConfiguration {
+    @Autowired
+    private WebExceptionHandlerProperties webExceptionHandlerProperties;
+
+    @ExceptionHandler(value = FeignException.class)
+    public RestfulApiResponse bindException(FeignException e) {
+        int state = RestfulApiResponse.ERROR_STATE;
+        Exception ex = e;
+        if (e.getCause() instanceof BusinessException) {
+            state = RestfulApiResponse.FAILURE_STATE;
+            ex = (Exception) e.getCause();
+        }
+        ErrorMessage message = ExceptionUtils.extractErrorMessage(ex);
+        return RestfulApiResponseBuilder.generic(state, message.getCode())
+                .data(null)
+                .msg(message.getMessage())
+                .friendlyMsg(message.getDisplayMessage())
+                .stackTrack(getStackTrace(message))
+                .build();
+    }
+
+    protected String getStackTrace(ErrorMessage message) {
+        return webExceptionHandlerProperties.getOutputStackTrace() ? message.getStackTrace() : "check log for details";
+    }
+}
